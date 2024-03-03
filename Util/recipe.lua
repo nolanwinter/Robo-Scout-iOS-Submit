c14732040ea7a8fbe48dfca026cec9d0f976c9db
nolanwinter <nolanwinter@users.noreply.github.com>
2024-03-02 21:55:33 -0500

    Update app name in recipe.lua

--- Util/recipe.lua
+++ Util/recipe.lua
@@ -2,7 +2,7 @@
 local root = os.getenv('GITHUB_WORKSPACE') or os.getenv('WORKSPACE')
 
 return {
-	appName = "Solar2Demo",
+	appName = "RRScout2024",
 	platform = "ios",
 	appVersion = os.getenv('APP_VERSION') or "1.0",
 	projectPath = root .. "/Project",
